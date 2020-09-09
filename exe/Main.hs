14fbfbcb67e25819d148027a93b78efc52d0ccb2
Albert González-Marín <dsifriend@gmail.com>
2020-09-09 18:41:25 +0200

    Added interactivity back in.

--- exe/Main.hs
+++ exe/Main.hs
@@ -1,12 +1,11 @@
 module Main where
 
--- import System.Environment
+import System.Environment
 import PrimeSum
 
 main :: IO ()
 main = do
-    print $ primeSum 10000
---    args <- getArgs
---    let i = read (head args) :: Integer
---    print $ primeSum i
+   args <- getArgs
+   let i = read (head args) :: Integer
+   print $ primeSum i
     
